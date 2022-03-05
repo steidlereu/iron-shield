a1bd87afdf200ccc7902212161f850519dd6c9b2
Felix <felix.steidler@steidler.eu>
2022-03-06 00:31:04 +0100

    fix stuff

--- security-smith/scripts/security-smith-installer.js
+++ security-smith/scripts/security-smith-installer.js
@@ -1,12 +1,14 @@
 const chalk = require('chalk');
 const fs = require('fs');
+const { exec } = require('child_process');
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 let securityConfig = {
-  grype: true
+  grype: false,
 };
 
 console.log(chalk.green('security smith installer'));
@@ -15,13 +17,30 @@ console.log(chalk.yellow('Please choose your vulnerability scanners:'));
 readline.question(`Install grype? (yes/no)`, answer => {
 
   if (answer === 'yes') {
-    console.log(`Hi `);
-  }
 
-  securityConfig.grype = false;
+    exec('curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin', (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+    securityConfig.grype = false;
+
+  }
 
   let data = JSON.stringify(securityConfig);
   fs.writeFileSync('config/custom/security.json', data);
+
+  readline.close();
+});
+
+readline.question(`Install clair? (yes/no)`, answer => {
 
   readline.close();
 });
